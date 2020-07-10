18c730c7b1c1ff0076709c60928618b81347660e
Alex2055 <alexfromnashville@gmail.com>
2020-07-09 23:26:22 -0500

    current wether block and functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,52 @@
+// show my City weather to fill the page on load
+window.addEventListener('DOMContentLoaded', myFunction);
+var myCity = "Nashville"
+
 function myFunction() {
-  var searchTerm = document.querySelector('#searchTerm').value;
-  // Create a variable to hold the value of rating
-  var rating = document.querySelector('#rating').value;
+
+
   fetch(
-    'https://api.giphy.com/v1/gifs/search?q=' +
-      searchTerm +
-      // Add the rating parameter
-      '&rating=' +
-      rating +
-      '&api_key=HvaacROi9w5oQCDYHSIk42eiDSIXH3FN&limit=1'
+    'https://api.openweathermap.org/data/2.5/weather?q=' + myCity + '&appid=8eda773abbe6b913d62bbf5d3cc23857&units=imperial'
   )
-    .then(function(response) {
+    .then(function (response) {
       return response.json();
     })
-    .then(function(response) {
-      console.log(response.data[0]);
-      var responseContainerEl = document.querySelector('#response-container');
-      responseContainerEl.innerHTML = '';
-      var gifImg = document.createElement('img');
-      gifImg.setAttribute('src', response.data[0].images.fixed_height.url);
-      responseContainerEl.appendChild(gifImg);
-    });
+    .then(function (response) {
+      console.log(response);
+      var showCity = document.getElementById("cityName");
+      showCity.innerHTML = response.name;
+
+      var skyCondition = document.getElementById("skyCond");
+      skyCondition.setAttribute('src', 'http://openweathermap.org/img/wn/' + response.weather[0].icon + '@2x.png')
+
+      var currentTemp = document.getElementById("Temp");
+      var tempInt = Math.round(response.main.temp);
+      currentTemp.innerHTML = "Temperature: " + tempInt;
+
+
+      var humiditi = document.getElementById("Humid");
+      var humidString = response.main.humidity.toString();
+      humiditi.innerHTML = "Humiditi: " + humidString;
+
+      var windspeed = document.getElementById("windSpeed");
+      var windFixed = response.wind.speed.toFixed(1);
+      windspeed.innerHTML = "Wind Speed: " + windFixed;
+
+      
+
+
+
+
+
+
+    })
+}
+
+
+function addnewCity() {
+  var newCity = document.getElementById("locationinput").value;
+
+  myCity = newCity;
+
+  myFunction();
 }
